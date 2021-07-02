9172c83aa522ca6783b6232b6a90ab34ed6a1950
Rem0ld <p.lovergne@hotmail.fr>
2021-07-02 10:07:44 +0200

    adding apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,66 @@
 import express from "express";
 import { Socket } from "socket.io";
-import { ChatroomManagerType, ChatroomType, Client } from "./types";
-import cors from "cors";
+import { ChatroomManagerType, ChatroomType, Client, ClientManagerType } from "./types";
+import { buildSchema } from "graphql";
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.set("port", port);
-const http = require("http").Server(app);
-// Binding socket.io to http server
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const typeDefs = gql(`
+type Query {
+  hello: String
+}`)
+
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Hello World!";
+    }
+  }
+}
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+const ClientManager = require("./socketControllers/ClientManager");
+const ChatroomManager = require("./socketControllers/chatroomManager");
+const makeHandlers = require("./socketControllers/handlers");
+
+const clientManager: ClientManagerType = ClientManager();
+const chatroomManager: ChatroomManagerType = ChatroomManager();
+
+
+// const client: Client = {
+//   id: "1192387",
+//   name: "Pierre",
+//   email: "p.lovergne@hotmail.fr",
+//   lastConnection: Date.now()
+// }
+// clientManager.addClient(client)
+// console.log(clientManager.getAllClients())
+
+// chatroomManager.addChatroom("salon1")
+// // console.log(chatroomManager.serializeChatrooms());
+// const salon: ChatroomType = chatroomManager.getChatroomByName("salon1");
+// salon.addMember(client)
+// salon.addEntry("test")
+// handleMessage({ chatroomName: "salon1", message: "je suis message" })
+// salon.getChatHistory();
+// // console.log(salon.getChatHistory())
+
+const http = app.listen(port, function () {
+  console.log(`Listening on localhost:${port}`)
+  console.log(`GraphQL API server at http://localhost:${port}/graphql`);
+});
 const io = require("socket.io")(http, {
   cors: {
     origin: "http://127.0.0.1:3001",
     methods: ["GET", "POST"]
   }
 });
-const ClientManager = require("./ClientManager");
-const ChatroomManager = require("./chatroomManager");
-const makeHandlers = require("./handlers");
-
-const clientManager = ClientManager();
-const chatroomManager = ChatroomManager();
-
-// app.get("/", (req: any, res: any) => {
-//   res.send("hello world");
-// });
 
 io.on('connection', function (socket: Socket) {
   const {
@@ -59,25 +96,3 @@ io.on('connection', function (socket: Socket) {
     console.log(err)
   })
 })
-
-// const client: Client = {
-//   id: "1192387",
-//   name: "Pierre",
-//   email: "p.lovergne@hotmail.fr",
-//   lastConnection: Date.now()
-// }
-// clientManager.addClient(client)
-// console.log(clientManager.getAllClients())
-
-// chatroomManager.addChatroom("salon1")
-// // console.log(chatroomManager.serializeChatrooms());
-// const salon: ChatroomType = chatroomManager.getChatroomByName("salon1");
-// salon.addMember(client)
-// salon.addEntry("test")
-// handleMessage({ chatroomName: "salon1", message: "je suis message" })
-// salon.getChatHistory();
-// // console.log(salon.getChatHistory())
-
-const server = http.listen(port, function () {
-  console.log(`Listening on localhost:${port}`);
-});
